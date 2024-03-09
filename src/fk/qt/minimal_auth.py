ca02f37f689a5f88e31f42c4c339248d9a16e8dc
Constantine <setwindowtext@gmail.com>
2024-03-09 16:25:14 +0100

    Simplified minimal_auth

--- src/fk/qt/minimal_auth.py
+++ src/fk/qt/minimal_auth.py
@@ -13,74 +13,15 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-import base64
-import json
 
-from PySide6.QtCore import QUrl
-from PySide6.QtGui import QDesktopServices
-from PySide6.QtNetwork import QNetworkAccessManager
-from PySide6.QtNetworkAuth import QOAuth2AuthorizationCodeFlow, QOAuthHttpServerReplyHandler, QAbstractOAuth
 from PySide6.QtWidgets import QPushButton
 
-from fk.qt.minimal_common import window, main_loop
-
-client_id = '248052959881-pqd62jj04427c7amt7g72crmu591rip8.apps.googleusercontent.com'
-client_secret = '...'
-local_port = 18889
-auth_url = 'https://accounts.google.com/o/oauth2/auth'
-token_url = 'https://oauth2.googleapis.com/token'
-
-
-def fix_parameters(stage, parameters):
-    if stage == QAbstractOAuth.Stage.RequestingAccessToken:
-        parameters['client_id'] = [client_id]
-        parameters['client_secret'] = [client_secret]
-        parameters['code'] = [QUrl.fromPercentEncoding(parameters['code'][0])]
-        parameters['redirect_uri'] = [f'http://127.0.0.1:{local_port}/']
-    elif stage == QAbstractOAuth.Stage.RequestingAuthorization:
-        parameters['access_type'] = ['offline']
-    return parameters
-
-
-def login():
-    mgr = QNetworkAccessManager(window)
-    flow = QOAuth2AuthorizationCodeFlow(client_id, auth_url, token_url, mgr, window)
-    flow.setScope('email')
-    flow.setClientIdentifierSharedKey(client_secret)
-    flow.authorizeWithBrowser.connect(QDesktopServices.openUrl)
-    flow.setReplyHandler(QOAuthHttpServerReplyHandler(local_port, window))
-    flow.setModifyParametersFunction(fix_parameters)
-    flow.granted.connect(lambda: pp(flow))
-    flow.grant()
-
-
-def refresh():
-    mgr = QNetworkAccessManager(window)
-    flow = QOAuth2AuthorizationCodeFlow(client_id, auth_url, token_url, mgr, window)
-    flow.setScope('email')
-    flow.setClientIdentifierSharedKey(client_secret)
-    flow.setRefreshToken('1//03NqJ3kgUh3GACgYIARAAGAMSNwF-L9Ir7UR7re7_ZqmK5ZgSIr5JcNpu7V6_t9pv3iKI1TBfI_z9_sPgjvJnvZHPljKqLL0XHMk')
-    flow.authorizeWithBrowser.connect(QDesktopServices.openUrl)
-    flow.setReplyHandler(QOAuthHttpServerReplyHandler(local_port, window))
-    flow.setModifyParametersFunction(fix_parameters)
-    flow.granted.connect(lambda: pp(flow))
-    flow.refreshAccessToken()
-
-
-def pp(flow: QOAuth2AuthorizationCodeFlow):
-    print('Access token:', flow.token())
-    id_token = flow.extraTokens().get('id_token', None)
-    print('ID token:', id_token)
-    b = bytes(id_token.split('.')[1], 'iso8859-1')
-    t = json.loads(base64.decodebytes(b + b'===='))
-    email = t['email']
-    print('Email:', email)
-    print('Refresh token:', flow.refreshToken())
-
+from fk.qt.minimal_common import window, main_loop, app
+from fk.qt.oauth import authenticate
 
 button = QPushButton(window)
 button.setText('Login...')
-button.clicked.connect(refresh)
+button.clicked.connect(lambda: authenticate(app, print))
 window.setCentralWidget(button)
 
 main_loop()
