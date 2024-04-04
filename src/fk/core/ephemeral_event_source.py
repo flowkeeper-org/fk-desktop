4b122e166a926c964029a725330feb24d56a2536
Constantine <setwindowtext@gmail.com>
2024-04-04 22:08:27 +0200

    Working ephemeral event source

--- src/fk/core/ephemeral_event_source.py
+++ src/fk/core/ephemeral_event_source.py
@@ -19,84 +19,34 @@ from fk.core import events
 from fk.core.abstract_event_source import AbstractEventSource
 from fk.core.abstract_settings import AbstractSettings
 from fk.core.abstract_strategy import AbstractStrategy
-from fk.core.strategy_factory import strategy_from_string
 
 TRoot = TypeVar('TRoot')
 
 
 class EphemeralEventSource(AbstractEventSource[TRoot]):
     _data: TRoot
-    _existing_strategies: Iterable[AbstractStrategy] | None
-    _last_strategy: AbstractStrategy | None
     _content: list[str]
 
     def __init__(self,
                  settings: AbstractSettings,
-                 root: TRoot,
-                 existing_strategies: Iterable[AbstractStrategy] | None = None):
+                 root: TRoot):
         super().__init__(settings)
         self._data = root
-        self._existing_strategies = existing_strategies
-        self._last_strategy = None
         self._content = list()
-
-    def get_last_strategy(self) -> AbstractStrategy | None:
-        return self._last_strategy
 
     def start(self, mute_events=True) -> None:
         print('Ephemeral event source -- starting')
-        if self._existing_strategies is None:
-            self._process_from_file(mute_events)
-        else:
-            self._process_from_existing()
-
-    def _process_from_existing(self) -> None:
-        # This method is called when we repair an existing data source
-        # All events are muted during processing
-        self._emit(events.SourceMessagesRequested, dict())
-        self.mute()
-        is_first = True
-        seq = 1
-        for strategy in self._existing_strategies:
-            strategy._data = self._data
-            strategy._settings = self._settings
-            strategy._emit_func = self._emit
-            if type(strategy) is str:
-                continue
-            self._last_strategy = strategy
-
-            if is_first:
-                is_first = False
-            else:
-                seq = strategy.get_sequence()
-                if seq != self._last_seq + 1:
-                    self._sequence_error(self._last_seq, seq)
-            self._last_seq = seq
-            self.execute_prepared_strategy(strategy)
-        self.auto_seal()
-        self.unmute()
-        self._emit(events.SourceMessagesProcessed, dict())
-
-    def _process_from_file(self, mute_events=True) -> None:
         self._emit(events.SourceMessagesRequested, dict())
         if mute_events:
             self.mute()
 
-        s = self.get_data().get_init_strategy(self._emit)
-        self._content.append(f'{s}')
-
-        strategy = strategy_from_string(s, self._emit, self.get_data(), self._settings)
-        self._last_strategy = strategy
-        self._last_seq = strategy.get_sequence()
+        strategy = self.get_data().get_init_strategy(self._emit)
+        self._content.append(f'{strategy}')
         self.execute_prepared_strategy(strategy)
-        self.auto_seal()
 
         if mute_events:
             self.unmute()
         self._emit(events.SourceMessagesProcessed, dict())
-
-    def repair(self) -> Iterable[str]:
-        return ['Not implemented']
 
     def _append(self, strategies: list[AbstractStrategy]) -> None:
         for s in strategies:
