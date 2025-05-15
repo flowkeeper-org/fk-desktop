6d948886490ef2330755d19ea596b2dff83f4b9f
co-stig <setwindowtext@gmail.com>
2025-05-15 11:31:59 +0200

    VT-scan

--- scripts/windows/vtscan.py
+++ scripts/windows/vtscan.py
@@ -22,52 +22,63 @@ import vt
 
 
 async def get_files_to_upload(queue, path):
-  """Finds which files will be uploaded to VirusTotal."""
-  files = path.split(',')
-  for f in files:
-    await queue.put(f)
-  return len(files)
+    """Finds which files will be uploaded to VirusTotal."""
+    files = path.split(',')
+    for f in files:
+        await queue.put(f)
+    return len(files)
 
 
 async def upload_hashes(queue, apikey):
-  """Uploads selected files to VirusTotal."""
-  return_values = []
+    """Uploads selected files to VirusTotal."""
+    return_values = []
 
-  async with vt.Client(apikey) as client:
-    while not queue.empty():
-      file_path = await queue.get()
-      with open(file_path, 'rb') as f:
-        analysis = await client.scan_file_async(file=f)
-        print(f"File {file_path} uploaded.")
-        queue.task_done()
-        return_values.append((analysis, file_path))
+    async with vt.Client(apikey) as client:
+        while not queue.empty():
+            file_path = await queue.get()
+            with open(file_path, 'rb') as f:
+                analysis = await client.scan_file_async(file=f)
+                print(f"File {file_path} uploaded.")
+                queue.task_done()
+                return_values.append((analysis, file_path))
 
-  return return_values
+    return return_values
 
 
 async def process_analysis_results(apikey, analysis, file_path):
-  async with vt.Client(apikey) as client:
-    completed_analysis = await client.wait_for_analysis_completion(analysis)
-    print(f"{file_path}: {completed_analysis.stats}")
-    print(f"analysis id: {completed_analysis.id}")
+    async with vt.Client(apikey) as client:
+        completed_analysis = await client.wait_for_analysis_completion(analysis)
+        detected = list()
+        # See https://docs.virustotal.com/reference/analyses-object
+        print(f"{file_path}: {completed_analysis.stats}")
+        print(f"analysis id: {completed_analysis.id}")
+        results = completed_analysis.results
+        print(f"results: {results}")
+        for engine in results:
+            result = results[engine]
+            # See https://virustotal.github.io/vt-py/_modules/vt/object.html
+            if result['category'] == 'malicious':
+                detected.append(f' - {engine}: {result["category"]} - {result["result"]}')
+        overall = 'Positive detections:\n' if len(detected) > 0 else 'Clean.'
+
 
 
 async def main(key: str, path: str):
-  queue = asyncio.Queue()
-  n_files = await get_files_to_upload(queue, path)
+    queue = asyncio.Queue()
+    n_files = await get_files_to_upload(queue, path)
 
-  worker_tasks = []
-  for _ in range(min(4, n_files)):
-    worker_tasks.append(asyncio.create_task(upload_hashes(queue, key)))
+    worker_tasks = []
+    for _ in range(min(4, n_files)):
+        worker_tasks.append(asyncio.create_task(upload_hashes(queue, key)))
 
-  # Wait until all worker tasks has completed.
-  analyses = itertools.chain.from_iterable(await asyncio.gather(*worker_tasks))
-  await asyncio.gather(
-      *[
-          asyncio.create_task(process_analysis_results(key, a, f))
-          for a, f in analyses
-      ]
-  )
+    # Wait until all worker tasks has completed.
+    analyses = itertools.chain.from_iterable(await asyncio.gather(*worker_tasks))
+    await asyncio.gather(
+        *[
+            asyncio.create_task(process_analysis_results(key, a, f))
+            for a, f in analyses
+        ]
+    )
 
 
 if __name__ == '__main__':
