97f324ba0ea9b789f26c2871a7fb7f959d9ae02a
Constantine <setwindowtext@gmail.com>
2025-08-31 13:12:33 +0200

    Default bg image

--- src/fk/core/abstract_settings.py
+++ src/fk/core/abstract_settings.py
@@ -280,7 +280,7 @@ class AbstractSettings(AbstractEventEmitter, ABC):
                     "gradient:Gradient",
                 ], _always_show),
                 # UC-3: Setting "Background image" is only shown if "Header background" = "Image"
-                ('Application.eyecandy_image', 'file', 'Background image', '', ['*.png;*.jpg'], _show_for_image_eyecandy),
+                ('Application.eyecandy_image', 'file', 'Background image', ':/img/bg.jpg', ['*.png;*.jpg'], _show_for_image_eyecandy),
                 # UC-3: Setting "Color scheme" and button "Surprise me!" are only shown if "Header background" = "Gradient"
                 ('Application.eyecandy_gradient', 'choice', 'Color scheme', 'NorseBeauty', ['NorseBeauty:NorseBeauty'], _show_for_gradient_eyecandy),
                 ('Application.eyecandy_gradient_generate', 'button', 'Surprise me!', '', [], _show_for_gradient_eyecandy),
